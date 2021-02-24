dca31fb16af690c20e3fda6d76d4960a78144804
Anthony Leon <Acidknights89@gmail.com>
2021-02-24 04:32:36 -0500

    Add relationship logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :inventory_lists
+    has_many :items, through: :inventory_lists
 
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
