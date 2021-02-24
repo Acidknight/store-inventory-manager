b6b9d6ce64117809e4e2577d0533c52ceb326636
Anthony Leon <Acidknights89@gmail.com>
2021-02-24 04:54:47 -0500

    Add new routes logic

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
 
+  resources :inventory_lists do
+    resources :items, only: [:new, :create, :index]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
