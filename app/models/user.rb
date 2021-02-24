c8c722f9d0d4b08c12eb6228c3d740fd1f9f6902
Anthony Leon <Acidknights89@gmail.com>
2021-02-24 04:42:35 -0500

    Change wording on relationship logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :inventory_lists
-    has_many :items, through: :inventory_lists
+    has_many :items
+    has_many :inventory_lists, through: :items
 
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
