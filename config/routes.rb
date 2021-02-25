6f9a9222181cc1d116b8f78aeee64c0518fe89f7
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 02:21:12 -0500

    Generate comments table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :items
   resources :inventory_lists
   root 'sessions#home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_075728) do
+ActiveRecord::Schema.define(version: 2021_02_25_072044) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "list_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "inventory_lists", force: :cascade do |t|
     t.string "name"
