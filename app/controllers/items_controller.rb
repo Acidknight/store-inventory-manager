5b794f8edd9637a88492c87c1d6c44cd8ded9c44
Anthony Leon <Acidknights89@gmail.com>
2021-02-24 20:30:03 -0500

    Add methods to create new items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+
+    def new 
+        @item = Item.new 
+    end
+
+    def create
+        @item = Item.new(item_params(:name, :category, :quantity, :location))
+        if @item.save 
+            redirect_to item_path(@item)
+        else
+            render new
+        end
+    end
 end
