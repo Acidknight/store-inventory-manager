f2ebfe79af8ed2fcdfda472b4dce55e456f419bb
Anthony Leon <Acidknights89@gmail.com>
2021-02-26 03:19:09 -0500

    Add editing a list logic

--- app/models/inventory_list.rb
+++ app/models/inventory_list.rb
@@ -3,5 +3,7 @@ class InventoryList < ApplicationRecord
     has_many :comments
     has_many :users_comments, through: :comments, source: :user
 
+    scope :alpha, -> { order('LOWER(name)') }
+
     validates :name, :item_count, :date, :time, :list_content, presence: true
 end
