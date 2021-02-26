9bafdc1e5a94186e48b4a3c9089ff41c5c2ad548
Anthony Leon <Acidknights89@gmail.com>
2021-02-26 16:36:33 -0500

    Add new and create methods for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,26 @@ class CommentsController < ApplicationController
             @comments = Comment.all
         end
     end
+
+    def new
+        if params[:list_id] && @inventory_list = InventoryList.find_by_id(params[:list_id])
+            @comment = @inventory_list.comments.new
+            
+        else
+            @error = "Inventory List doesn't exist" if params[:list_id]
+            @comment = Comment.new
+        end
+    end
+
+    def create
+        @inventory_list = InventoryList.find(params[:comment][:list_id])
+        @comment = current_user.comments.new(comment_params)
+        
+        if @comment.save
+            redirect_to inventory_list_comment_path(@inventory_list, @comment)
+        else
+            @error = @comment.errors.full_messages
+            render :new
+        end
+    end
 end
