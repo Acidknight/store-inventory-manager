0a9b7db99b7dee7565939a97ff65ccecb54e4c7b
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 23:44:37 -0500

    Remove code that is not used/refactoring code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
 
    
-    def new 
-        @user =User.new
-        render :login
-
+    def home 
+        @user = User.find_by_id(params[:id])
     end
 
     def create 
-        @user = User.find_by(username: params[:user][:username])
+        @user = User.find_by_id(params[:id])
         if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user_id
+            session[:user_id] = @user.id
             redirect_to users_path(@user)
         else
             redirect_to '/login'
