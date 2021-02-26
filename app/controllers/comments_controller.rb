01d0177acfa95b1b7391d427a83bdd37a5516832
Anthony Leon <Acidknights89@gmail.com>
2021-02-26 15:45:17 -0500

    Add index method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+   
+    def index
+        if params[:inventory_list_id] && @inventory_list = InventoryList.find_by_id(params[:inventory_list_id])
+            @comments = @inventory_list.comments
+        elsif current_user
+            @comments = current_user.comments.all
+        else
+            @error = "Inventory List doesn't exist" if params[:inventory_list]
+            @comments = Comment.all
+        end
+    end
 end
