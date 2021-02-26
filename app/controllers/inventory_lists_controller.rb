c2a625184b047d4c90fbbef9c2cc24b8ce8d3b41
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 23:33:55 -0500

    Add index method

--- app/controllers/inventory_lists_controller.rb
+++ app/controllers/inventory_lists_controller.rb
@@ -1,2 +1,10 @@
 class InventoryListsController < ApplicationController
+
+    def index
+        if params[:i]
+            @inventory_lists = InventoryList.search(params[:i])
+        else
+            @inventory_lists = InventoryList.alpha.ApplicationController
+        end
+    end
 end
