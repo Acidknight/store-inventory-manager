a54b94c253c147229eb29a2425e2c669fa09c65a
Anthony Leon <Acidknights89@gmail.com>
2021-02-25 23:45:44 -0500

    Refactor destroy method code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,13 +16,9 @@ class SessionsController < ApplicationController
 
     end
 
-    def home 
-
-    end
-
     def destroy
-        session.clear
-        redirect_to '/'
+        session.delete :user_id
+        redirect_to root_path
     end
 
 end
