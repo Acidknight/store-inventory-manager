dff16e4140c5a3781076b7dc7e710c3ece2a5203
Anthony Leon <Acidknights89@gmail.com>
2021-02-27 23:30:51 -0500

    Fix users being able to login without email input

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
+        @user = User.find_by(email: params[:user][:email])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id 
             redirect_to user_path(@user) 
         else
-            flash[:error] = "Sorry, your username or password were incorrect."
+            flash[:error] = "Sorry, your username, email, or password were incorrect."
             redirect_to '/login'
         end
     end
